1a9a09db3d99dc6d31f412f1592bb7f39fb13e9a
etoto06 <etoto06@naver.com>
2024-02-22 12:59:19 +0900

    push

--- 03/03.js
+++ 03/03.js
@@ -1,6 +1,25 @@
- function handleClick(n){
-    document.querySelector("#msgArea").innerHTML = "<h2>버튼" + n + "이 눌러졌습니다</h2>";  //!@!@! id앞에 # 기억 
-    // document.getElementById("msgArea").innerHTML="안녕하세요";
-    //document.querySelector("#msgArea").innerHTML = `<h2>버튼 ${n} 이 눌러졌습니다</h2>` ;  // !@!@! ${n} 중괄호 기억
-
+ /*
+ 함수 작성 2 : 화살표 함수 
+ */
+//     함수명      파라미터
+ const handleClick = (n) => {
+    document.querySelector("#msgArea").innerHTML = `<h2>버튼 ${n} 이 눌러졌습니다</h2>` ;
  }
+
+
+
+
+
+
+
+
+// /*
+//  함수 작성 1
+//  */
+ 
+//  function handleClick(n){
+//     document.querySelector("#msgArea").innerHTML = "<h2>버튼" + n + "이 눌러졌습니다</h2>";  //!@!@! id앞에 # 기억 
+//     // document.getElementById("msgArea").innerHTML="안녕하세요";
+//     //document.querySelector("#msgArea").innerHTML = `<h2>버튼 ${n} 이 눌러졌습니다</h2>` ;  // !@!@! ${n} 중괄호 기억
+
+//  }
