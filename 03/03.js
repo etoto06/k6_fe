d983b4647219f5855b27807d5bfb670150bb64b7
etoto06 <etoto06@naver.com>
2024-02-22 12:54:53 +0900

    push

--- 03/03.js
+++ 03/03.js
@@ -1,6 +1,6 @@
  function handleClick(n){
-    document.querySelector("#msgArea").innerHTML = "<h2>버튼" + n + "이 눌러졌습니다</h2>";
+    document.querySelector("#msgArea").innerHTML = "<h2>버튼" + n + "이 눌러졌습니다</h2>";  //!@!@! id앞에 # 기억 
     // document.getElementById("msgArea").innerHTML="안녕하세요";
-    //document.querySelector("#msgArea").innerHTML = `<h2>버튼 ${n} 이 눌러졌습니다</h2>` ;
+    //document.querySelector("#msgArea").innerHTML = `<h2>버튼 ${n} 이 눌러졌습니다</h2>` ;  // !@!@! ${n} 중괄호 기억
 
  }
