8225721fcd2096f9e6d77e59108b1996e321d412
etoto06 <etoto06@naver.com>
2024-02-22 13:07:15 +0900

    push

--- 03/03.js
+++ 03/03.js
@@ -3,7 +3,14 @@
  */
 //     함수명      파라미터
  const handleClick = (n) => {
-    document.querySelector("#msgArea").innerHTML = `<h2>버튼 ${n} 이 눌러졌습니다</h2>` ;
+
+    let msg;
+    if (n ==1) msg ="<h2>안녕하세요</h2>";
+    else msg ="<h2>안녕히가세요 </h2>" ;
+
+    document.querySelector("#msgArea").innerHTML =msg ;
+
+   // document.querySelector("#msgArea").innerHTML = `<h2>버튼 ${n} 이 눌러졌습니다</h2>` ;
  }
 
 
